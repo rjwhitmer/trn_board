dd7f4a40deb4df769fd88d3f2a8262eb2d3f9aa5
Bob <rjwhitmercc@gmail.com>
2024-01-15 12:09:02 -0700

    Create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,32 @@
 
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
+
+  def new; end
+
+  def create
+    @user = User.new
+
+    @user.update(
+      name: params[:name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+
+    if @user.save!
+      flash.now[:alert] = 'User successfully created!'
+      redirect_to action: 'index'
+    else
+      flash.now[:error] = 'Something went wrong. Try again'
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def delete; end
 end
